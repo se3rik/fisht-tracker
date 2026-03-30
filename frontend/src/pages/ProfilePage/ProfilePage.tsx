1d4b346579cc04529ecbb04ede79716fca9b3b44
se3rik <se3rik@mail.ru>
2026-03-30 08:33:10 +0300

    feat(frontend): filtering specialties depending on the department

--- frontend/src/pages/ProfilePage/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage/ProfilePage.tsx
@@ -10,20 +10,16 @@ import { useAppSelector } from '@/hooks/useAppSelector';
 import { getProfileData, updateProfileData } from '@/stores/slices/profileSlice';
 
 import { PageHeading } from '@/components/pageHeading/PageHeading';
+import { BaseInput } from '@/components/ui/BaseInput/BaseInput';
+import { BaseSelect } from '@/components/ui/BaseSelect/BaseSelect';
 
 import { stringAvatar } from '@/helpers/stringAvatar';
 
 import { DEPARTMENT_LABELS } from '@/constants/departmentsLabels';
 import { SPECIALTY_LABELS } from '@/constants/specialityLabels';
-import { BaseInput } from '@/components/ui/BaseInput/BaseInput';
-import { BaseSelect } from '@/components/ui/BaseSelect/BaseSelect';
+import { DEPARTMENT_SPECIALTY_MAP } from '@/constants/departmentSpecialityMap';
 
 const departmentItems = Object.entries(DEPARTMENT_LABELS).map(([value, title], id) => ({
-    id,
-    value,
-    title,
-}));
-const specialityItems = Object.entries(SPECIALTY_LABELS).map(([value, title], id) => ({
     id,
     value,
     title,
@@ -34,7 +30,7 @@ export const ProfilePage = () => {
     const { profileData } = useAppSelector((state) => state.profile);
 
     const [isEditing, setIsEditing] = useState<boolean>(false);
-    const { register, reset, handleSubmit, control } = useForm({
+    const { register, reset, handleSubmit, control, watch } = useForm({
         defaultValues: {
             secondName: '',
             firstName: '',
@@ -44,6 +40,20 @@ export const ProfilePage = () => {
             speciality: '',
         },
     });
+    const selectedDepartment = watch('department');
+
+    const filteredSpecialtyItems =
+        selectedDepartment && DEPARTMENT_SPECIALTY_MAP[selectedDepartment]
+            ? DEPARTMENT_SPECIALTY_MAP[selectedDepartment].map((value, index) => ({
+                  id: index,
+                  value,
+                  title: SPECIALTY_LABELS[value],
+              }))
+            : Object.entries(SPECIALTY_LABELS).map(([value, title], index) => ({
+                  id: index,
+                  value,
+                  title,
+              }));
 
     useEffect(() => {
         dispatch(getProfileData());
@@ -52,12 +62,12 @@ export const ProfilePage = () => {
     useEffect(() => {
         if (profileData) {
             reset({
-                secondName: profileData.secondName ?? 'Не указано',
-                firstName: profileData.firstName ?? 'Не указано',
-                patronymic: profileData.patronymic ?? 'Не указано',
-                email: profileData.email ?? 'Не указано',
-                department: profileData.department ?? 'Не указано',
-                speciality: profileData.speciality ?? 'Не указано',
+                secondName: profileData.secondName ?? '',
+                firstName: profileData.firstName ?? '',
+                patronymic: profileData.patronymic ?? '',
+                email: profileData.email ?? '',
+                department: profileData.department ?? '',
+                speciality: profileData.speciality ?? '',
             });
         }
     }, [profileData, reset]);
@@ -147,7 +157,7 @@ export const ProfilePage = () => {
                                         id="speciality"
                                         label="Должность"
                                         disabled={!isEditing}
-                                        menuItems={specialityItems}
+                                        menuItems={filteredSpecialtyItems}
                                     />
                                 )}
                             />
